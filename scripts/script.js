5be8282968d28b37149efa0666bac6c4dcace349
chiamaka28 <maduikechiamaka4@gmail.com>
2023-01-25 14:17:22 +0100

    some changes to the css file

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,8 @@ const menu = document.querySelector('ul');
 const leftBtn = document.querySelector('#left-btn');
 const rightBtn = document.getElementById('right-btn');
 // const text = document.querySelector('.slider-text');
-const img = document.querySelectorAll('.main-img');
+// const images = document.querySelector('.slider-image')
+const img = document.getElementsByClassName('.main-img');
 const text = document.querySelectorAll('.text');
 
 
@@ -11,9 +12,60 @@ const text = document.querySelectorAll('.text');
 btn.addEventListener('click', function (){
   btn.classList.toggle('active');
    menu.classList.toggle('show');
+    
+
 })
 
 
+
+// let slidePosition = 0;
+// const totalSlides = slides.length;
+// console.log(totalSlides);
+
+// rightBtn.addEventListener('click', function() {
+//  moveToNextSlide()
+ 
+// })
+
+// leftBtn.addEventListener('click', () => {
+//  moveToPreviousSlide()
+
+// })
+
+
+
+// function moveToNextSlide() {
+  
+//   if(slidePosition === totalSlides - 1){
+//     slidePosition = 0;
+//   }else{
+//     slidePosition++;
+//   }
+//  updateSlidePosition();
+  
+// };
+
+// function moveToPreviousSlide() {
+//   if (slidePosition === 0){
+//     slidePosition = totalSlides - 1;
+//   }else{
+//     slidePosition--;
+//   }
+//   updateSlidePosition();
+ 
+// };
+// function updateSlidePosition(){
+//   // for (let slide of slides) {
+//   //   console.log(slide)
+//   //   slide.classList.remove('show');
+//   //   slide.classList.add("hidden");
+//   // }
+//   slides.forEach(slide => {
+//     slide.classList.remove('show');
+//     slide.classList.add("hidden");
+//   });
+//    slides[slidePosition].classList.add("show");
+//   }
 // showSlides(i);
 
 
@@ -45,36 +97,37 @@ btn.addEventListener('click', function (){
 
 //    rightBtn.addEventListener('click', () => {
  
-//   if (i > 1) {i = img.length - 1}
-//   if (i < 1) {i = img.length}
+//   if (i > 1) {i = slides.length - 1}
+//   if (i < 1) {i = slides.length}
 
-//   for (let i = 0; i < img.length;  i++ ) {
-//       img[i].style.display = "none";
-//       console.log(img.length);
+//   for (let i = 0; i < slides.length;  i++ ) {
+//       slides[i].style.display = "none";
+//       console.log(slides.length);
 //       console.log(i)
      
 //   } 
-//   img[i - 1].style.display = "block";
+//   slides[i - 1].style.display = "block";
  
 //    });
 
-
-let currentElementIndex = 0;
-rightBtn.addEventListener('click', () => {
-  // console.log('clicked')
-for (let i = 0; i < img.length; i++) {
-  if(i === currentElementIndex){
-    img[i].style.display = "none";
-  ;
-  console.log(i)
-  }else{
-    img[(i) % img.length].style.display = "block";
-   
-  }
+//////////////////////////////////////
+// let currentElementIndex = 0;
+// rightBtn.addEventListener('click', () => {
+//   console.log('clicked')
+// for (let i = 0; i < slides.length; i++) {
+//   if(i === currentElementIndex){
+//     slides[i].style.display = "none";
   
-}
-});
-
+//   console.log(i);
+//   }else{
+//     img[(i) % slides.length].style.display = "block";
+   
+//   }}}
+// ) ;
+////////////////////////////////////////////
+// }
+// });
+//////////////////////////////////////////////////////////////////////////
 
  let index = 0;
 
@@ -93,18 +146,18 @@ function previmg() {
 
  
 }
-  
+//////////////////////////////////////////////////////////////////////////////////////////////  
 
-// function nextimg() {
-//   img[index % img.length].classList.replace('show', 'hidden');
-//   img[(index + 1) % img.length].classList.replace('hidden', 'show');
-//   text[index % text.length].classList.replace('show', 'hide');
-//   text[(index + 1) % text.length].classList.replace('hide', 'show');
-//   index++;
-// }
+function nextimg() {
+  img[index % img.length].classList.replace('show', 'hidden');
+  img[(index + 1) % img.length].classList.replace('hidden', 'show');
+  text[index % text.length].classList.replace('show', 'hide');
+  text[(index + 1) % text.length].classList.replace('hide', 'show');
+  index++;
+}
 
 leftBtn.addEventListener('click', previmg);
-// rightBtn.addEventListener('click', nextimg);
+rightBtn.addEventListener('click', nextimg);
 
 
 
