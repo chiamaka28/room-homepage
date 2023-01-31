3ef9be8726c6aee14ae19a12f5ccdfd75492b0a9
chiamaka28 <maduikechiamaka4@gmail.com>
2023-01-31 10:31:55 +0100

    t

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,8 @@ const btn = document.querySelector('.toggle');
 const menu = document.querySelector('ul');
 const leftBtn = document.querySelector('#left-btn');
 const rightBtn = document.getElementById('right-btn');
-// const text = document.querySelector('.slider-text');
-// const images = document.querySelector('.slider-image')
-const img = document.getElementsByClassName('.main-img');
+// const text = document.querySelector('section');
+const img = document.querySelectorAll('.main-img');
 const text = document.querySelectorAll('.text');
 
 
@@ -17,6 +16,55 @@ btn.addEventListener('click', function (){
 })
 
 
+let index = 0;
+let i = 0;
+
+function next() {
+  img[index].classList.remove('show');
+  index = (index + 1) % img.length;
+  img[index].classList.add('show');
+
+ 
+  
+}
+
+
+function nexttxt() {
+  console.log(text.length);
+  text[i].classList.remove('active');
+  i = (i + 1) % text.length;
+  text[i].classList.add('active');
+}
+
+
+
+
+
+
+function prev() {
+  img[index].classList.remove('show');
+  index = (index - 1 + img.length) % img.length;
+  img[index].classList.add('show');  
+}
+
+function prevtxt() {
+  console.log(text.length);
+  text[i].classList.remove('active');
+  i = (i - 1 + text.length) % text.length;
+  text[i].classList.add('active');
+}
+
+
+leftBtn.addEventListener('click', function () {
+  // prev();
+  prevtxt();
+});
+
+
+rightBtn.addEventListener('click', function() {
+  next();
+  nexttxt();
+});
 
 // let slidePosition = 0;
 // const totalSlides = slides.length;
@@ -129,35 +177,35 @@ btn.addEventListener('click', function (){
 // });
 //////////////////////////////////////////////////////////////////////////
 
- let index = 0;
+//  let index = 0;
 
-function previmg() {
+// function previmg() {
 
-  if (index === 0){
-    index = img.length;
+//   if (index === 0){
+//     index = img.length;
    
- }
+//  }
  
-  img[index % img.length].classList.replace('show', 'hidden');
-  img[(index - 1) % img.length].classList.replace('hidden', 'show');
-  text[index % text.length].classList.replace('show', 'hide');
-  text[(index - 1) % text.length].classList.replace('hide', 'show');
-  index--;
+//   img[index % img.length].classList.replace('show', 'hidden');
+//   img[(index - 1) % img.length].classList.replace('hidden', 'show');
+//   text[index % text.length].classList.replace('show', 'hide');
+//   text[(index - 1) % text.length].classList.replace('hide', 'show');
+//   index--;
 
  
-}
+// }
 //////////////////////////////////////////////////////////////////////////////////////////////  
 
-function nextimg() {
-  img[index % img.length].classList.replace('show', 'hidden');
-  img[(index + 1) % img.length].classList.replace('hidden', 'show');
-  text[index % text.length].classList.replace('show', 'hide');
-  text[(index + 1) % text.length].classList.replace('hide', 'show');
-  index++;
-}
+// function nextimg() {
+//   img[index % img.length].classList.replace('show', 'hidden');
+//   img[(index + 1) % img.length].classList.replace('hidden', 'show');
+//   text[index % text.length].classList.replace('show', 'hide');
+//   text[(index + 1) % text.length].classList.replace('hide', 'show');
+//   index++;
+// }
 
-leftBtn.addEventListener('click', previmg);
-rightBtn.addEventListener('click', nextimg);
+// leftBtn.addEventListener('click', previmg);
+// rightBtn.addEventListener('click', nextimg);
 
 
 
